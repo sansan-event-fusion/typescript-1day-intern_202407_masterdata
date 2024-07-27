ac38fb333e07020c2bd73c218b6c545334fb0d6d
穂積優真 <hozumiyuma@hodzumiyuushinnoMacBook-Pro.local>
2024-07-27 17:12:01 +0900

    名寄せ・統合処理の実装

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 正規化ワークフロー
-  console.time('normalizeWorkflow');
-  const rawRepo = app.get(RawDataRepo);//リポジトリ=データの永続化場所のインスタンス化
-  const rawDataObservable = await rawRepo.fetch();//csvデータ取得
-  const normalizeExecutor = app.get<WorkFlowExecutorService>(
-    WorkflowType.NORMALIZE,
-  );
-  await normalizeExecutor.execute(rawDataObservable); //ワークフロー実行
-  console.timeEnd('normalizeWorkflow');
+  // console.time('normalizeWorkflow');
+  // const rawRepo = app.get(RawDataRepo);//リポジトリ=データの永続化場所のインスタンス化
+  // const rawDataObservable = await rawRepo.fetch();//csvデータ取得
+  // const normalizeExecutor = app.get<WorkFlowExecutorService>(
+  //   WorkflowType.NORMALIZE,
+  // );
+  // await normalizeExecutor.execute(rawDataObservable); //ワークフロー実行
+  // console.timeEnd('normalizeWorkflow');
 
   // 名寄せ・統合ワークフロー
   console.time('mergeWorkflow');
