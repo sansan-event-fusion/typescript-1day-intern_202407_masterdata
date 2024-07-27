d254533abf7f0b0795a9f294b5583f152b925891
穂積優真 <hozumiyuma@hodzumiyuushinnoMacBook-Pro.local>
2024-07-27 15:10:36 +0900

    制御文字やBOMを取り除く処理を追加

--- src/application-services/normalize/steps/normalize-phone-number.step.ts
+++ src/application-services/normalize/steps/normalize-phone-number.step.ts
@@ -1,6 +1,7 @@
 import { NormalizeWorkflowStep } from 'src/types/normalize-workflow-step';
 import { Attributes } from 'src/value/attribute';
 import { PhoneNumberAttributeValue } from 'src/value/business-location-attribute';
+import { CONTROL_CHARACTER_REGEXP, CJK_RADICALS_SUPPLEMENT_REPLACE_REGEXP_MAP } from './constant';
 
 export const NormalizePhoneNumberStep: NormalizeWorkflowStep = (data) => {
   if (!data.in.phone_number) return data;
@@ -24,5 +25,15 @@ export const NormalizePhoneNumberStep: NormalizeWorkflowStep = (data) => {
 
 const normalizePhoneNumber = (phoneNumber: string) => {
   // ここに処理を書いてください
-  return phoneNumber;
+  // 全角ハイフンを半角ハイフンに変換する
+  let normalizedPhoneNumber = phoneNumber.replace(/\u30FC/g, '-');
+  normalizedPhoneNumber =  normalizedPhoneNumber.replace(/[０-９]/g, function(s) {
+    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+  });
+
+  normalizedPhoneNumber = normalizedPhoneNumber.replace(/\s/g, "");
+
+  normalizedPhoneNumber = normalizedPhoneNumber.replace(CONTROL_CHARACTER_REGEXP, "")
+
+  return normalizedPhoneNumber;
 };
